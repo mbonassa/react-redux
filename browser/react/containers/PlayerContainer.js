fe33efa4e9a988d8ccf36d94b9053179f85c7753
Marcelo Bonassa <marcelo.bonassa@gmail.com>
2017-05-22 15:56:01 -0400

    changes

--- browser/react/containers/PlayerContainer.js
+++ browser/react/containers/PlayerContainer.js
@@ -1,56 +1,75 @@
-import React, {Component} from 'react';
 import AUDIO from '../audio';
-import store from '../store';
 import {previous, next, setProgress, toggleSong} from '../action-creators/player';
 import Player from '../components/Player';
+import {connect} from 'react-redux';
 
-class PlayerContainer extends Component {
-
-  constructor() {
-    super();
-    this.state = store.getState().player;
-    this.toggle = this.toggle.bind(this);
-  }
-
-  componentDidMount() {
-
-    AUDIO.addEventListener('ended', this.next);
-    AUDIO.addEventListener('timeupdate', () => {
-      store.dispatch(setProgress(AUDIO.currentTime / AUDIO.duration));
-    });
-
-    this.unsubscribe = store.subscribe(() => {
-      this.setState(store.getState().player);
-    });
-  }
-
-  componentWillUnmount() {
-    this.unsubscribe();
-  }
-
-  next() {
-    store.dispatch(next());
-  }
-
-  prev() {
-    store.dispatch(previous());
-  }
-
-  toggle() {
-    store.dispatch(
-      toggleSong(this.state.currentSong, this.state.currentSongList)
-    );
-  }
-
-  render() {
-    return <Player
-      {...this.state}
-      next={this.next}
-      prev={this.prev}
-      toggle={this.toggle}
-    />;
-  }
-
+const mapPropsToState = (state, ownProps) => {
+  return Object.assign({}, state.player)
 }
 
-export default PlayerContainer;
+const mapDispatchToProps = (dispatch, ownProps) => ({
+  next: () => {
+    dispatch(next());
+  },
+  prev: () => {
+    dispatch(previous());
+  },
+  toggle: () => {
+    dispatch(
+      toggleSong(ownProps.currentSong, ownProps.currentSongList)
+    )
+  } 
+})
+
+export default connect(mapPropsToState, mapDispatchToProps)(Player)
+
+// class PlayerContainer extends Component {
+
+//   constructor() {
+//     super();
+//     this.state = store.getState().player;
+//     this.toggle = this.toggle.bind(this);
+//   }
+
+//   componentDidMount() {
+
+//     AUDIO.addEventListener('ended', this.next);
+//     AUDIO.addEventListener('timeupdate', () => {
+//       store.dispatch(setProgress(AUDIO.currentTime / AUDIO.duration));
+//     });
+
+//     this.unsubscribe = store.subscribe(() => {
+//       this.setState(store.getState().player);
+//     });
+//   }
+
+//   componentWillUnmount() {
+//     this.unsubscribe();
+//   }
+
+//   next() {
+//     store.dispatch(next());
+//   }
+
+//   prev() {
+//     store.dispatch(previous());
+//   }
+
+//   toggle() {
+//     store.dispatch(
+//       toggleSong(this.state.currentSong, this.state.currentSongList)
+//     );
+//   }
+
+//   render() {
+//     return <Player
+//       {...this.state}
+//       next={this.next}
+//       prev={this.prev}
+//       toggle={this.toggle}
+//     />;
+//   }
+
+// }
+
+// export default PlayerContainer;
